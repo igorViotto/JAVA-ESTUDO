7764f7cd164f335f6584b47c158d4b683c8fbd12
igor.viotto <igorcviotto@gmail.com>
2025-02-12 09:03:01 -0300

    #

--- src/com/agibank/semana04/aula1002/exercicios/exerc06.java
+++ src/com/agibank/semana04/aula1002/exercicios/exerc06.java
@@ -4,39 +4,40 @@ public class exerc06 {
     public static void main(String[] args) {
 
         //todo, CONJUNTO DE 5 VALORES
-        double[] retornos = {0.01, 0.02, -0.01, 0.03, -0.02};
+        double[] valores = {0.05, 0.02, -0.08, 0.03, -0.015};
 
-        System.out.printf("Volatidade: %.4f%n", calcularVolatidade(retornos));
+        System.out.printf("Volatidade: %.4f%n", calcularVolatidade(valores));
 
     }
 
-    static double calcularVolatidade(double[] retornos) {
+    static double calcularVolatidade(double[] valores) {
 
         double soma = 0;
 
-        //todo, CÁLCULO DA MÉDIA
-        for (int i = 0; i < retornos.length; i++){
-            soma += retornos[i];//SOMA
+        //todo, CÁLCULO SOMA
+        for (int i = 0; i < valores.length; i++) {
+            soma += valores[i];//SOMA
         }
 
-        double calcMedia = soma / 5;//MÉDIA
-        //Todo, FIM MÉDIA
+        //Todo, CALCULO MÉDIA
+        double media = soma / 5;//MÉDIA
 
-        //todo, GUARDA OS VALORES DA DE
-        double[] desvioAoQuadrado = new double[retornos.length];
+        //todo, GUARDA OS VALORES DESVIO² (linha '31')
+        double[] desvioAoQuadrado = new double[valores.length];
 
-        //todo, FOR QUE PERCORRE OS ÍNDICES DO VETOR 'desvio * desvio', QUE SERIA DESVIO AO QUADRADO
-        for (int i = 0; i < retornos.length; i++){
-            double desvio = retornos[i] - calcMedia;//todo, FAZ CADA ÍNDICE DO VETOR 'retorno[]' SUBTRAIR A 'média'
+        //todo, FOR QUE PERCORRE OS ÍNDICES DO VETOR 'desvio²'
+        for (int i = 0; i < valores.length; i++){
+            double desvio = valores[i] - media;//todo, FAZ CADA ÍNDICE DO VETOR 'retorno[]' SUBTRAIR A 'média'
             desvioAoQuadrado[i] = desvio * desvio;//todo, CADA ÍNDICE DO VETOR 'desvioAoQuadrado[]', RECEBE TODOS OS RESULTADOS DA SUBTRAÇÃO AO QUADRADO
         }
 
+        //fixme - Arumar o método 'calcularVolatidade'
         double somaDesvio = 0;
-        for (int i = 0; i < retornos.length; i++){
+        for (int i = 0; i < valores.length; i++){
             somaDesvio += desvioAoQuadrado[i];//todo, SOMA TODOS OS VALORES DO VETOR 'desvioAoQuadrado[]'E GUARDA EM 'somaDesvio'
         }
 
-        double variancia = somaDesvio / retornos.length;//todo, DEVIDE O VALOR TOTAL DA SOMA DE 'somaDesvio' PELA QUANTIDADE DE INDICES NO VETOR 'retorno', = 5
+        double variancia = somaDesvio / valores.length;//todo, DEVIDE O VALOR TOTAL DA SOMA DE 'somaDesvio' PELA QUANTIDADE DE INDICES NO VETOR 'retorno', = 5
 
         return Math.sqrt(variancia);//todo, 'Math.sqrt' ACHA A RAIZ QUADRADA DA VARIANCIA (SOMADESVIO / 5)
     }
